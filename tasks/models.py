c18fce50b9cb6a4c0d758fec670aabcd969fa30c
Peterson Paulo <63976046+peterpaulodev@users.noreply.github.com>
2021-05-24 00:34:24 -0300

    Primeiro commit

---
+++ tasks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+
+    STATUS = (
+        ('doing', 'Doing'),
+        ('done', 'Done'),
+    )
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.CharField(max_length=5, choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self): # Retorna o 'title' como nome do registro no admin
+        return self.title
---
+++ tasks/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
+
+def taskList(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
+
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addTask.html', {'form': form})
+
+def helloWorld(request):
+    return HttpResponse('Hello world!')
+
+def yourName(request, name):
+    return render(request, 'tasks/yourname.html', {'name': name})
